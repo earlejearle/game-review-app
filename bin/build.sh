7166928d696505b140f473371ad22242645204c9
Jai Earle <jaiearley@hotmail.com>
2023-12-21 13:33:20 +1100

    Adding logs to build

--- bin/build.sh
+++ bin/build.sh
@@ -3,10 +3,16 @@
 set -o errexit
 
 # Add build commands for front end
+echo "Deleting public directory"
 rm -rf public
+echo "Running npm install and build for client"
 npm install --prefix client && npm run build --prefix client
+echo "copy build to public"
 cp -a client/build/. public/
 
+echo "Starting bundle install"
 bundle install
+echo "Running rake db:migrate"
 bundle exec rake db:migrate
+echo "Running rails db:seed"
 bundle exec rails db:seed
